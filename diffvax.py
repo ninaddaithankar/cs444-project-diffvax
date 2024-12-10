fixed a mistake in masking

diffvax.py
@@ -4,8 +4,9 @@ import argparse
 import torch
 import torch.optim as optim
 
-from diffusers import StableDiffusionInpaintPipeline
 from tqdm import tqdm
+from torchvision import transforms
+from diffusers import StableDiffusionInpaintPipeline
 
 from utils.models import *
 from utils.losses import L_noise, L_edit
@@ -43,7 +44,13 @@ def train(args):
 
 
 	# -- transformations
-	shared_transforms = []
+	shared_transforms = transforms.Compose([
+		transforms.Resize((512, 512)),
+		transforms.ToTensor(),
+
+		# Optionally normalize the image based on model's training norm (mean, std)
+		# transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # For Stable Diffusion model normalization
+	])
 
 
 	# -- init the dataset

utils/dataset.py
@@ -93,7 +93,7 @@ class CC2_Dataset(torch.utils.data.Dataset):
 
         # so we simply keep the background as 0 and set other areas to 1 and treat the 1 are as foreground
         mask[mask != 0] = 1
-        mask = np.where(mask == 1, 0, 1)
+        # mask = np.where(mask == 1, 0, 1)
 
         prompt = random.choice(self.prompts)
 

utils/losses.py
@@ -9,6 +9,8 @@ def L_noise(immunized_image, original_image, mask):
 
 # -- the Ledit term - drives the edited mask image to zero
 def L_edit(immunized_image, original_image, mask, editing_model, prompt):
-    # Have to define this editing model, stable diffusion inpainting
-    edited_image = editing_model(immunized_image, mask, prompt)
+    
+    # flip the mask to paint the background and pass it through the stable diffusion pipeline
+    edited_image = editing_model(immunized_image, 1 - mask, prompt)
+
     return torch.sum(torch.abs(edited_image - original_image) * (1 - mask)) / torch.sum(1 - mask)